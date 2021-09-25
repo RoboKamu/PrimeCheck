50f760a4fa7b686b29213e48b1b9f71ef8416231
RoboKamu <91156959+RoboKamu@users.noreply.github.com>
2021-09-25 17:29:13 +0200

    Java solution (WIP)

---
+++ primtal.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class primtal {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        //ask for input
+        System.out.println("Ange ett heltal: ");
+        int num = input.nextInt();
+        
+        //call the object prime to start the sequence, and see if input is prime
+        prime(num);
+        
+        //see if the returned value is None(not prime) or not None(prime)
+        if (prime(num) == true){
+            System.out.println("Det är ett primtal");
+        }
+        
+        else{
+            System.out.println("Det är inte ett primtal");
+        }
+        
+        
+    }
+    
+    public static boolean prime(int num){
+        Scanner input = new Scanner(System.in);
+        
+        //make an list from 0 up to num
+        int numList[] = new int[num];
+        for (int z = 0; z < num; z++){
+            numList[z] = z;
+    }
+        
+        //loop through number and calculate if some multiply to the num
+        for (int i = 0; i < num; i++){
+            for (int n = 0; n < num-1; n++){
+                if (i*numList[n] == num){
+                    return true;
+                }
+            }
+        }
+    }
+}
