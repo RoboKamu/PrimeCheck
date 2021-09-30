28907ea3121afbb787ef7028c00fb0394264e3e9
RoboKamu <91156959+RoboKamu@users.noreply.github.com>
2021-09-30 23:22:50 +0200

    primtal Java

--- primtal.java
+++ primtal.java
@@ -4,39 +4,39 @@ public class primtal {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         //ask for input
-        System.out.println("Ange ett heltal: ");
+        System.out.println("Enter an integer: ");
         int num = input.nextInt();
         
         //call the object prime to start the sequence, and see if input is prime
-        prime(num);
+        int prime = prime(num);
         
-        //see if the returned value is None(not prime) or not None(prime)
-        if (prime(num) == true){
-            System.out.println("Det är ett primtal");
+        //see if the returned value is True or False
+        if (prime == true){
+            System.out.println("It is a prime");
         }
         else{
-            System.out.println("Det är inte ett primtal");
+            System.out.println("It is not a prime");
         }
         
         
     }
     
     public static boolean prime(int num){
-        Scanner input = new Scanner(System.in);
-        
         //make an list from 0 up to num
-        int numList[] = new int[num];
-        for (int z = 0; z < num; z++){
+        int[] numList = new int[num];
+        for (int z = 0; z <= num-1; z++){
             numList[z] = z;
-    }
-        
-        //loop through number and calculate if some multiply to the num
-        for (int i = 0; i < num; i++){
-            for (int n = 0; n < num-1; n++){
-                if (i*numList[n] == num){
-                    return true;
-                }
-            }
         }
+        
+        //loop through all numbers up to num, and calculate which 2 multiply together
+        for (int i = 1; i <= num-1; i++){
+            for (int n = 1; n <= num-1; n++){
+                if (i*numList[n] == num){
+                    //2 numbers that are not 1 or num, multiply to num
+                    return false;
+                }
+            }            
+        }
+        return true;
     }
 }
